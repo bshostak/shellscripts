83923e1d8f884db3b34978ddce4a2e3044254903
Brian Shostak (Salt Lake City) <Brian.Shostak@usanainc.com>
2022-06-08 12:55:55 -0600

    first

---
+++ setup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#To execute, save this file and then cd to where this is saved. Run, `chmod +x ./setup.sh` and then `./setup.sh`
+
+#Install Homebrew
+echo "Installing Homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+#Install Cask
+echo "Installing brew cask..."
+brew tap homebrew/cask
+
+#Install Programming Languages
+echo "Installing Programming Languages.."
+brew install --cask typescript
+brew install --cask corretto
+
+#Dev Related Tools
+echo "Installing Dev Related Tools..."
+brew install git
+brew install node
+brew install nvm
+#Now set default version
+echo "Setting default node version..."
+echo "Specify which default node version you want:"
+READ version
+nvm install $version
+nvm alias default v$version
+if [ ! -d "/Applications/IntelliJ IDEA.app" ]; then
+    brew install --cask intellij-idea
+fi
+if [ ! -d "/Applications/Visual Studio Code.app" ]; then
+    brew install --cask visual-studio-code
+fi
+if [ ! -d "/Applications/Postman.app" ]; then
+    brew install --cask postman
+fi 
+
+#Browsers
+echo "Installing Web Browsers..."
+if [ ! -d "/Applications/Firefox.app" ]; then
+    brew install --cask firefox
+fi 
+if [ ! -d "/Applications/Google Chrome.app" ]; then
+    brew install --cask google-chrome
+fi 
+if [ ! -d "/Applications/Microsoft Edge.app" ]; then
+    brew install --cask microsoft-edge
+fi
+
+#Communcation Related Apps
+echo "Installing Communication Apps..."
+if [ ! -d "/Applications/Slack.app" ]; then
+    brew install --cask slack
+fi
+
+#Other things I use daily-ish
+if [ ! -d "/Applications/OBS.app" ]; then
+    brew install --cask obs
+fi
